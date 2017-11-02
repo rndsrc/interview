e9a41af830ebccd877841ec5b47d5e20b72cf6d6
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-11-02 19:37:42 -0400

    Load "io" from "interview"

---
+++ interview/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2017 Chi-kwan Chan
+# Copyright (C) 2017 Harvard-Smithsonian Center for Astrophysics
+#
+# This file is part of interview.
+#
+# Interview is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Interview is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with interview.  If not, see <http://www.gnu.org/licenses/>.
+
+from . import io
