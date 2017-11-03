c75cb5b4c697d72f1312c923f59839b2d056bad6
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-11-03 01:39:51 -0400

    Create an empty scatter plot

--- examples/demo.py
+++ examples/demo.py
@@ -8,6 +8,9 @@
 import sys
 import numpy as np
 
+import bokeh.models   as bm
+import bokeh.plotting as bp
+
 from eat.io import hops, util
 
 if len(sys.argv) > 1:
@@ -30,3 +33,26 @@ df['site2'] = df.baseline.str[1]
 util.add_path(df)
 
 df["r"] = np.sqrt(df.u**2 + df.v**2)
+
+# Create empty Bokeh column data source with column names matching the
+# pandas data frmae
+src = bm.ColumnDataSource(data={k:[] for k in df.columns})
+
+# Create hover tool with some useful information; use it for a Bokeh
+# figure; create a scatter plot
+hover = bm.HoverTool(tooltips=[
+    ("Baseline",     "@site1 @site2"),
+    ("(u,v)",        "(@u, @v)"),
+    ("Polarization", "@pol"),
+    ("Path",         "@path"),
+])
+fig = bp.figure(title="Scatter plot",
+                plot_height=720, plot_width=720,
+                toolbar_location="above", tools=[hover,
+                "pan,box_zoom,box_select,lasso_select,undo,redo,reset,save"],
+                output_backend="webgl")
+plt = fig.circle(x="datetime", y="phase", source=src, size=5)
+
+# Add everything to the root
+bp.curdoc().add_root(fig)
+bp.curdoc().title = "Demo"
