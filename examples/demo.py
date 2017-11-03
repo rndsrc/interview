e21255c21986a8312a8b838b5a5ac4cdbfabdae8
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-11-03 01:20:36 -0400

    Read an alist file, rename and add some columns

--- examples/demo.py
+++ examples/demo.py
@@ -6,6 +6,9 @@
 # where FILE is the file to be opened.
 
 import sys
+import numpy as np
+
+from eat.io import hops, util
 
 if len(sys.argv) > 1:
     files = sys.argv[1:]
@@ -13,3 +16,17 @@ else:
     files = ["er1/hops-lo/5.+close/data/alist.v6"]
 
 print('Inspecting file{} "{}"'.format('s' if len(files) > 1 else '', files))
+
+# Read an alist file
+df = hops.read_alist(files[0])
+df.rename(columns={
+    'polarization': "pol",
+    'ref_freq'    : "freq",
+    'resid_phas'  : "phase",
+}, inplace=True)
+
+df['site1'] = df.baseline.str[0]
+df['site2'] = df.baseline.str[1]
+util.add_path(df)
+
+df["r"] = np.sqrt(df.u**2 + df.v**2)
